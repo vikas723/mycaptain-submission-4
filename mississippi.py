dedac1aedbaa3b9a38384c1c8088580e3590de7d
vikas723 <81210333+vikas723@users.noreply.github.com>
2021-04-16 22:58:29 +0530

    Add files via upload

---
+++ mississippi.py
@@ -0,0 +1,10 @@
+w=input('please enter a string')
+def most_frequent(string):
+    d = dict()
+    for key in string:
+        if key not in d:
+            d[key] = 1
+        else:
+            d[key] += 1
+    return d
+print (most_frequent('mississippi'))
